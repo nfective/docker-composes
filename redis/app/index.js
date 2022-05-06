beb86ad75d29962398461ff43ea66c0c46ec91e4
Nathan Thomas <thomanat@iu.edu>
2022-05-05 23:09:21 -0400

    Created working example of redis and app

---
+++ redis/app/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const redis = require("redis")
+
+const app = express()
+const client = redis.createClient({
+    host:"redis-server",
+    port: 6379,
+    password: "foobar"
+})
+client.set('visits', 0)
+
+app.get("/", (req, res) => {
+    client.get("visits", (err, visits ) =>
+    {
+        res.send(`number of visits: ${ visits }`)
+        client.set("visits", parseInt(visits) + 1)
+    })
+})
+
+app.listen(8080, () => {
+    console.log("Running on port 8080")
+})
