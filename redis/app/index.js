b33646c681cde2ff663a5760cf03314cdc7dec0e
Nathan Thomas <thomanat@iu.edu>
2022-05-06 00:20:41 -0400

    Adding gitignore

--- redis/app/index.js
+++ redis/app/index.js
@@ -3,7 +3,7 @@ const redis = require("redis")
 
 const app = express()
 const client = redis.createClient({
-    host:"redis-server",
+    host:"localhost",
     port: 6379,
     password: "foobar"
 })
